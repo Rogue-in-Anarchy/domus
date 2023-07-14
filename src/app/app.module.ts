d1cb1ae0791b3bb9b54fb5b31dd0dccc3f3816d7
nehemiah-ttfltd <nejeh@ttfltd.com>
2023-07-14 16:02:58 +0100

    slider updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProjectDetailsComponent } from './project-details/project-details.component'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     NgxGalleryModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  slides = [
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-daliss5.png"},
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-colin72-12_crop600x600topcenter.png"},
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-francis42-14.png"},
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-duncan9.png"},
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-catherine17-r.png"},
+    {img: "https://en.elmarkstore.eu/data/uploads/moxesImages/955-herald15.png"}
+  ];
+  slideConfig = {"slidesToShow": 3, "slidesToScroll": 1, "autoplay": true};
+  slidersConfig = {"slidesToShow": 2, "slidesToScroll": 1, "autoplay": true};
+  sliderConfig = {"slidesToShow": 1, "slidesToScroll": 1, "autoplay": true};
+
+   
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
